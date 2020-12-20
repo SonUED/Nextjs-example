6f90a47c82ab6b0ab8e15121c00b206a186c7357
SonUED <45086369+SonUED@users.noreply.github.com>
2020-12-20 11:33:17 +0700

    finished dynamic routes

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,15 +1,25 @@
+import Head from "next/head";
 import Layout from "../../components/layout";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
 export default function Post({ postData }) {
   return (
-    <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-    </Layout>
+    <>
+      <Layout>
+        <Head>
+          <title>{postData.title}</title>
+        </Head>
+        <article>
+          <h1 className={utilStyles.headingX}>{postData.title}</h1>
+          <div className={utilStyles.lightText}>
+            <Date dateString={postData.date}></Date>
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
+      </Layout>
+    </>
   );
 }
 export async function getStaticPaths() {
@@ -20,7 +30,7 @@ export async function getStaticPaths() {
   };
 }
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
